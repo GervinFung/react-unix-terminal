a09da8b8988801f676a6c1f56436d129ec21009b
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2022-07-31 22:39:13 +0800

    chore(dep): removed `Makefile` & used `scripts` instead. Mandatorize `scripts` (#8)

--- src/theme/Theme.d.ts
+++ src/theme/Theme.d.ts
@@ -2,5 +2,6 @@ import 'styled-components';
 import Theme from './Theme';
 
 declare module 'styled-components' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     export interface DefaultTheme extends Theme {}
 }
