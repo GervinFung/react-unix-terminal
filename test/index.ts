aa885094fa67f342c07944a836ddb9fd3aec06e2
PoolOfDeath20 <53143214+GervinFung@users.noreply.github.com>
2022-10-18 01:47:58 +0800

    Standardize test (#9)
    * test(standardize): add `only` from test
    
    * fix(build): use `build-prod` instead of `build`

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,17 @@ import testDefaultCommand from './command/defaultCommand';
 import testCommandHistoryParser from './command/parser';
 import { testCommandCompletion, testShell } from './command/util';
 
-testCommandHistoryParser();
-testCommandCompletion();
-testShell();
-testDefaultCommand();
+const tests: ReadonlyArray<Readonly<[() => void, 'only'?]>> = [
+    [testCommandHistoryParser],
+    [testCommandCompletion],
+    [testShell],
+    [testDefaultCommand],
+];
+
+const selectedTests = tests.filter(([_, only]) => only);
+
+if (process.env.IS_CI && selectedTests.length) {
+    throw new Error('cannot have "only" in ci cd');
+}
+
+(!selectedTests.length ? tests : selectedTests).forEach(([test]) => test());
